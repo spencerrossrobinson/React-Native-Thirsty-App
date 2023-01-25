cb369c51f546c946268fe4fc66a5b7e4fac7a07d
Spencer Robinson <spencerwarmouth@gmail.com>
2023-01-25 14:10:08 -0500

    Find my cocktail page Rum and Tequila addition

--- screens/SelectScreen.js
+++ screens/SelectScreen.js
@@ -19,6 +19,7 @@ import { COCKTAIL } from "../shared/cocktail";
 import * as Animatable from "react-native-animatable";
 
 const SelectScreen = ({ navigation }) => {
+  //gin
   const filterGin = COCKTAIL.filter((value) => value.type === "gin");
   const filterGinBalanced = COCKTAIL.filter(
     (value) => value.taste === "balanced" && value.type === "gin"
@@ -26,6 +27,7 @@ const SelectScreen = ({ navigation }) => {
   const filterGinBoozy = COCKTAIL.filter(
     (value) => value.taste === "boozy" && value.type === "gin"
   );
+  //whiskey
   const filterWhiskey = COCKTAIL.filter((value) => value.type === "whiskey");
   const filterWhiskeyBalanced = COCKTAIL.filter(
     (value) => value.taste === "balanced" && value.type === "whiskey"
@@ -33,8 +35,27 @@ const SelectScreen = ({ navigation }) => {
   const filterWhiskeyBoozy = COCKTAIL.filter(
     (value) => value.taste === "boozy" && value.type === "whiskey"
   );
+  //rum
+  const filterRum = COCKTAIL.filter((value) => value.type === "rum");
+  const filterRumBalanced = COCKTAIL.filter(
+    (value) => value.taste === "balanced" && value.type === "rum"
+  );
+  const filterRumBoozy = COCKTAIL.filter(
+    (value) => value.taste === "boozy" && value.type === "rum"
+  );
+  //tequila
+  const filterTequila = COCKTAIL.filter((value) => value.type === "tequila");
+  const filterTequilaBalanced = COCKTAIL.filter(
+    (value) => value.taste === "balanced" && value.type === "tequila"
+  );
+  const filterTequilaBoozy = COCKTAIL.filter(
+    (value) => value.taste === "boozy" && value.type === "tequila"
+  );
+
   const [gin, setGin] = useState(false);
   const [whiskey, setWhiskey] = useState(false);
+  const [rum, setRum] = useState(false);
+  const [tequila, setTequila] = useState(false);
   const [balanced, setBalanced] = useState(false);
   const [boozy, setBoozy] = useState(false);
 
@@ -67,6 +88,50 @@ const SelectScreen = ({ navigation }) => {
           <Card.Title style={styles.title}>Whiskey Cocktail Index</Card.Title>
           <Card.Divider />
           {filterWhiskey.map((value) => {
+            return (
+              <ListItem key={value.id}>
+                <Avatar size="xlarge" rounded source={value.img} />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.text}>
+                    {value.name}
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+            );
+          })}
+        </Card>
+      </Animatable.View>
+    );
+  };
+  const Rum = () => {
+    return (
+      <Animatable.View animation="fadeInDownBig" duration={1000} delay={500}>
+        <Card>
+          <Card.Title style={styles.title}>Rum Cocktail Index</Card.Title>
+          <Card.Divider />
+          {filterRum.map((value) => {
+            return (
+              <ListItem key={value.id}>
+                <Avatar size="xlarge" rounded source={value.img} />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.text}>
+                    {value.name}
+                  </ListItem.Title>
+                </ListItem.Content>
+              </ListItem>
+            );
+          })}
+        </Card>
+      </Animatable.View>
+    );
+  };
+  const Tequila = () => {
+    return (
+      <Animatable.View animation="fadeInDownBig" duration={1000} delay={500}>
+        <Card>
+          <Card.Title style={styles.title}>Tequila Cocktail Index</Card.Title>
+          <Card.Divider />
+          {filterTequila.map((value) => {
             return (
               <ListItem key={value.id}>
                 <Avatar size="xlarge" rounded source={value.img} />
@@ -411,7 +476,334 @@ const SelectScreen = ({ navigation }) => {
       );
     }
   };
-
+  const RumSelector = () => {
+    if (rum === true && balanced === true) {
+      return (
+        <Animatable.View animation="fadeInRightBig" duration={1000} delay={500}>
+          <Card
+            containerStyle={{
+              padding: 0,
+              borderRadius: 20,
+              margin: 10,
+              backgroundColor: "#e2711d",
+              borderColor: "white",
+              borderWidth: 5,
+            }}
+          >
+            {filterRumBalanced.map((value) => {
+              return (
+                <View
+                  key={value.id}
+                  style={{
+                    margin: 20,
+                    backgroundColor: "#ffb627",
+                    borderRadius: 20,
+                    borderColor: "white",
+                    borderWidth: 5,
+                  }}
+                >
+                  <Card.Image
+                    source={value.img}
+                    style={{
+                      borderTopLeftRadius: 20,
+                      borderTopRightRadius: 20,
+                    }}
+                  >
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                      <Text
+                        style={{
+                          color: "white",
+                          textAlign: "center",
+                          fontSize: 50,
+                        }}
+                      >
+                        {value.name}
+                      </Text>
+                    </View>
+                  </Card.Image>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Preperation: {value.prep}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Served: {value.style}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 10,
+                    }}
+                  >
+                    Recipe: {value.recipe}
+                  </Text>
+                </View>
+              );
+            })}
+          </Card>
+        </Animatable.View>
+      );
+    }
+    if (rum === true && boozy === true) {
+      return (
+        <Animatable.View animation="fadeInRightBig" duration={1000} delay={500}>
+          <Card
+            containerStyle={{
+              padding: 0,
+              borderRadius: 20,
+              margin: 10,
+              backgroundColor: "#e2711d",
+              borderColor: "white",
+              borderWidth: 5,
+            }}
+          >
+            {filterRumBoozy.map((value) => {
+              return (
+                <View
+                  key={value.id}
+                  style={{
+                    margin: 20,
+                    backgroundColor: "#ffb627",
+                    borderRadius: 20,
+                    borderColor: "white",
+                    borderWidth: 5,
+                  }}
+                >
+                  <Card.Image
+                    source={value.img}
+                    style={{
+                      borderTopLeftRadius: 20,
+                      borderTopRightRadius: 20,
+                    }}
+                  >
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                      <Text
+                        style={{
+                          color: "white",
+                          textAlign: "center",
+                          fontSize: 50,
+                        }}
+                      >
+                        {value.name}
+                      </Text>
+                    </View>
+                  </Card.Image>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Preparation: {value.prep}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Served: {value.style}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 10,
+                    }}
+                  >
+                    Recipe: {value.recipe}
+                  </Text>
+                </View>
+              );
+            })}
+          </Card>
+        </Animatable.View>
+      );
+    }
+  };
+  const TequilaSelector = () => {
+    if (tequila === true && balanced === true) {
+      return (
+        <Animatable.View animation="fadeInRightBig" duration={1000} delay={500}>
+          <Card
+            containerStyle={{
+              padding: 0,
+              borderRadius: 20,
+              margin: 10,
+              backgroundColor: "#e2711d",
+              borderColor: "white",
+              borderWidth: 5,
+            }}
+          >
+            {filterTequilaBalanced.map((value) => {
+              return (
+                <View
+                  key={value.id}
+                  style={{
+                    margin: 20,
+                    backgroundColor: "#ffb627",
+                    borderRadius: 20,
+                    borderColor: "white",
+                    borderWidth: 5,
+                  }}
+                >
+                  <Card.Image
+                    source={value.img}
+                    style={{
+                      borderTopLeftRadius: 20,
+                      borderTopRightRadius: 20,
+                    }}
+                  >
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                      <Text
+                        style={{
+                          color: "white",
+                          textAlign: "center",
+                          fontSize: 50,
+                        }}
+                      >
+                        {value.name}
+                      </Text>
+                    </View>
+                  </Card.Image>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Preperation: {value.prep}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Served: {value.style}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 10,
+                    }}
+                  >
+                    Recipe: {value.recipe}
+                  </Text>
+                </View>
+              );
+            })}
+          </Card>
+        </Animatable.View>
+      );
+    }
+    if (tequila === true && boozy === true) {
+      return (
+        <Animatable.View animation="fadeInRightBig" duration={1000} delay={500}>
+          <Card
+            containerStyle={{
+              padding: 0,
+              borderRadius: 20,
+              margin: 10,
+              backgroundColor: "#e2711d",
+              borderColor: "white",
+              borderWidth: 5,
+            }}
+          >
+            {filterTequilaBoozy.map((value) => {
+              return (
+                <View
+                  key={value.id}
+                  style={{
+                    margin: 20,
+                    backgroundColor: "#ffb627",
+                    borderRadius: 20,
+                    borderColor: "white",
+                    borderWidth: 5,
+                  }}
+                >
+                  <Card.Image
+                    source={value.img}
+                    style={{
+                      borderTopLeftRadius: 20,
+                      borderTopRightRadius: 20,
+                    }}
+                  >
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                      <Text
+                        style={{
+                          color: "white",
+                          textAlign: "center",
+                          fontSize: 50,
+                        }}
+                      >
+                        {value.name}
+                      </Text>
+                    </View>
+                  </Card.Image>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Preparation: {value.prep}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 20,
+                    }}
+                  >
+                    Served: {value.style}
+                  </Text>
+                  <Text
+                    style={{
+                      margin: 5,
+                      textAlign: "center",
+                      color: "white",
+                      fontSize: 10,
+                    }}
+                  >
+                    Recipe: {value.recipe}
+                  </Text>
+                </View>
+              );
+            })}
+          </Card>
+        </Animatable.View>
+      );
+    }
+  };
   const BalNBoozy = () => {
     return (
       <Animatable.View animation="fadeInRightBig" duration={1000} delay={500}>
@@ -460,10 +852,34 @@ const SelectScreen = ({ navigation }) => {
       );
     }
   };
+  const TestRum = () => {
+    if (rum === true) {
+      return (
+        <View>
+          <BalNBoozy />
+          <RumSelector />
+          <Rum />
+        </View>
+      );
+    }
+  };
+  const TestTequila = () => {
+    if (tequila === true) {
+      return (
+        <View>
+          <BalNBoozy />
+          <TequilaSelector />
+          <Tequila />
+        </View>
+      );
+    }
+  };
 
   const resetForm = () => {
     setGin(false);
     setWhiskey(false);
+    setRum(false);
+    setTequila(false);
     setBalanced(false);
     setBoozy(false);
   };
@@ -477,6 +893,8 @@ const SelectScreen = ({ navigation }) => {
         onPress={() => {
           setGin(true);
           setWhiskey(!true);
+          setRum(!true);
+          setTequila(!true);
         }}
       />
       <CheckBox
@@ -486,10 +904,36 @@ const SelectScreen = ({ navigation }) => {
         onPress={() => {
           setWhiskey(true);
           setGin(!true);
+          setRum(!true);
+          setTequila(!true);
+        }}
+      />
+      <CheckBox
+        center
+        title="Rum"
+        checked={rum}
+        onPress={() => {
+          setWhiskey(!true);
+          setGin(!true);
+          setRum(true);
+          setTequila(!true);
+        }}
+      />
+      <CheckBox
+        center
+        title="Tequila"
+        checked={tequila}
+        onPress={() => {
+          setWhiskey(!true);
+          setGin(!true);
+          setRum(!true);
+          setTequila(true);
         }}
       />
       <TestGin />
       <TestWhiskey />
+      <TestTequila />
+      <TestRum />
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate("Bar Directory")}
