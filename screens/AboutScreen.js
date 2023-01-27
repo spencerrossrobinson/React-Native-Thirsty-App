bfc41cc0ab99c994a56619bb6ed582cc7f91e52a
Spencer Robinson <spencerwarmouth@gmail.com>
2023-01-27 13:45:37 -0500

    Animations for About Page

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,40 +1,43 @@
 import { ScrollView, Text, View, StyleSheet } from "react-native";
 import { Card } from "react-native-elements";
 import { ABOUT } from "../shared/about";
+import * as Animatable from "react-native-animatable";
 
 const AboutScreen = () => {
   return (
     <ScrollView>
-      <Text style={styles.header}> Find Out About What Our App Offers</Text>
-      {ABOUT.map((value) => {
-        return (
-          <Card
-            containerStyle={{ padding: 0, borderRadius: 20 }}
-            key={value.id}
-          >
-            <Card.Image
-              source={value.image}
-              style={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+      <Animatable.View animation="bounceInDown" duration={1000} delay={500}>
+        <Text style={styles.header}> Find Out About What Our App Offers</Text>
+        {ABOUT.map((value) => {
+          return (
+            <Card
+              containerStyle={{ padding: 0, borderRadius: 20 }}
+              key={value.id}
             >
-              <View style={{ justifyContent: "center", flex: 1 }}>
-                <Text
-                  style={{
-                    color: "white",
-                    textAlign: "center",
-                    fontSize: 40,
-                    fontWeight: "bold",
-                  }}
-                >
-                  {value.name}
-                </Text>
-              </View>
-            </Card.Image>
-            <Text style={{ margin: 20, textAlign: "center" }}>
-              {value.description}
-            </Text>
-          </Card>
-        );
-      })}
+              <Card.Image
+                source={value.image}
+                style={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+              >
+                <View style={{ justifyContent: "center", flex: 1 }}>
+                  <Text
+                    style={{
+                      color: "white",
+                      textAlign: "center",
+                      fontSize: 40,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {value.name}
+                  </Text>
+                </View>
+              </Card.Image>
+              <Text style={{ margin: 20, textAlign: "center" }}>
+                {value.description}
+              </Text>
+            </Card>
+          );
+        })}
+      </Animatable.View>
     </ScrollView>
   );
 };
